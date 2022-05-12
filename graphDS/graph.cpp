b6061996c20d18e550f8985a31965f12aa0ef66e
arogya <arogyabichpuria16@gmail.com>
2022-05-12 16:58:00 +0530

    created the BFS traversal

---
+++ graphDS/graph.cpp
@@ -0,0 +1,98 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Graph
+{
+    vector<vector<int>> m_grapharr;
+
+public:
+    Graph(vector<vector<int>> graph)
+    {
+        m_grapharr = graph;
+    }
+
+    void setMatrix(vector<vector<int>> graph)
+    {
+        m_grapharr = graph;
+    }
+
+    void printGraph()
+    {
+        vector<vector<int>>::iterator outer;
+        vector<int>::iterator inner;
+
+        for (outer = m_grapharr.begin(); outer != m_grapharr.end(); outer++)
+        {
+            for (inner = outer->begin(); inner != outer->end(); inner++)
+            {
+                cout << *inner;
+            }
+
+            cout << endl;
+        }
+    }
+
+    vector<int> bfs()
+    {
+
+        queue<int> q;
+        vector<int> visited;
+        q.push(1);
+        while (!q.empty())
+        {
+
+            int front = q.front();
+            q.pop();
+
+            vector<int>::iterator itr;
+            vector<int>::iterator check;
+
+            check = std::find(visited.begin(), visited.end(), front);
+            if (check == visited.end())
+            {
+                visited.push_back(front);
+                for (itr = m_grapharr[front].begin(); itr != m_grapharr[front].end(); itr++)
+                {
+                    q.push(*itr);
+                }
+            }
+        }
+
+        return visited;
+    }
+
+    void dfs()
+    {
+    }
+};
+
+int main()
+{
+
+    vector<vector<int>> arrdouble{
+        {},
+        {4, 2},
+        {1, 5, 7, 8},
+        {4, 2, 10, 9},
+        {1, 3},
+        {6, 7, 2, 8},
+        {5},
+        {5, 8, 2},
+        {2, 7, 5},
+        {3},
+        {3}};
+
+    Graph s1(arrdouble);
+    s1.printGraph();
+    cout << "code is running fine." << endl;
+
+    vector<int> yo = s1.bfs();
+
+    vector<int>::iterator itr;
+
+    for (itr = yo.begin(); itr != yo.end(); itr++)
+    {
+        cout << *itr << endl;
+    }
+}
