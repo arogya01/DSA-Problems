f208f917d1168a8ccb787ef123a548facf948746
arogya bichpuria <arogyabichpuria16@gmail.com>
2023-10-08 16:50:42 +0530

    understood the right soln

--- single-threaded-cpu/main.cpp
+++ single-threaded-cpu/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
@@ -46,15 +47,60 @@ public:
         }
 
         vector<int> ans;
-        for (auto i : pq)
-        {
-            ans.push_back(i);
-        }
+        // for (auto i : pq)
+        // {
+        //     ans.push_back(i);
+        // }
 
         return ans;
     }
 };
 
+class BetterSolution
+{
+public:
+    vector<int> res;
+    vector<int> getOrder(vector<vector<int>> &tasks)
+    {
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> valid;
+        for (int i = 0; i < tasks.size(); i++)
+        {
+            pq.push({tasks[i][0], tasks[i][1], i});
+        }
+        long long timestamp = pq.top()[0];
+
+        while (!pq.empty() || !valid.empty())
+        {
+            while (!pq.empty() && timestamp >= pq.top()[0])
+            { // put all the valid tasks to the new pq
+                auto curr = pq.top();
+                pq.pop();
+                valid.push({curr[1], curr[2]});
+            }
+            // use the smallest valid tasks
+            if (valid.empty())
+            {
+                timestamp = pq.top()[0];
+                continue;
+            }
+            auto validTop = valid.top();
+            valid.pop();
+            res.push_back(validTop[1]);
+            timestamp += validTop[0];
+        }
+
+        return res;
+    }
+};
+
 int main()
 {
+    vector<vector<int>> tasks{{1, 2}, {2, 4}, {3, 2}, {4, 1}};
+    BetterSolution newS;
+
+    vector<int> res{newS.getOrder(tasks)};
+
+    for (auto i : res)
+        cout << i;
 }
