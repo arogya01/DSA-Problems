b4c3fbb766e14b05501519b404d2399bf17355a5
arogya <arogyabichpuria16@gmail.com>
2024-04-22 22:51:19 +0530

    learned about topological sort

---
+++ topological-sort/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <stack>
+
+using namespace std;
+
+class Solution
+{
+
+private:
+    void dfs(int node, int vis[], stack<int> st, vector<int> adj[])
+    {
+        vis[node] = 1;
+        for (auto it : adj[node])
+        {
+            if (!vis[it])
+                dfs(it, vis, st, adj);
+        }
+
+        st.push(node);
+    }
+
+public:
+    vector<int> topoSort(int V, vector<int> adj[])
+    {
+        int vis[V] = {0};
+        stack<int> st;
+
+        for (int i = 0; i < V; i++)
+        {
+            if (!vis[i])
+            {
+                dfs(i, vis, st, adj);
+            }
+        }
+        vector<int> ans;
+
+        while (!st.empty())
+        {
+            ans.push_back(st.top());
+            st.pop();
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    cout << "hello world yo" << endl;
+}
